9d4ac52b2dc259aee79940f455c267bccd84df73
Elda20 <eldha20@gmail.com>
2022-06-25 14:05:25 +0700

    Initial commit

---
+++ app/src/main/java/com/eldaauliana/hotelapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.eldaauliana.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView _imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        String imageUrl = "https://www.gotravelly.com/blog/wp-content/uploads/2020/01/Karma-Kandara.jpg";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+    }
+}
